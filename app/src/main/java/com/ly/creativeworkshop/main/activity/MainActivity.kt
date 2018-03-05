dea4837014edc807de2ea52b804e5612a9b903e9
kumi <876625596@qq.com>
2018-03-05 22:22:09 +0800

    a

--- app/src/main/java/com/ly/creativeworkshop/main/activity/MainActivity.kt
+++ app/src/main/java/com/ly/creativeworkshop/main/activity/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.ly.creativeworkshop.main
+package com.ly.creativeworkshop.main.activity
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
---
+++ app/src/main/java/com/ly/creativeworkshop/main/anko/MainAnko.kt
@@ -0,0 +1,7 @@
+package com.ly.creativeworkshop.main.anko
+
+/**
+ * Created by Administrator on 2018/3/5.
+ */
+class MainAnko {
+}
